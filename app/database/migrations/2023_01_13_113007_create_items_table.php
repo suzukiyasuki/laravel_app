4d336507100c5d1658f33ada09121d8065973bee
suzukiyasuki <y.suzuki1513@gmail.com>
2023-01-13 23:58:35 +0900

    first commit

---
+++ app/database/migrations/2023_01_13_113007_create_items_table.php
@@ -0,0 +1,38 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+class CreateItemsTable extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('items', function (Blueprint $table) {
+            $table->bigIncrements('id');
+            $table->integer('user_id');
+            $table->integer('category_id');
+            $table->string('name', '255');
+            $table->integer('amount');
+            $table->string('text', '255');
+            $table->string('image', '255');
+            $table->integer('size');
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('items');
+    }
+}
